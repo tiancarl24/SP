ee4caa45521ce73ae9117b459b91042be1b40783
Gimson Recilla <recillagimson@gmail.com>
2018-08-06 11:50:42 +0800

    ErrorFixHeader

--- HotelReservation/incHeader.php
+++ HotelReservation/incHeader.php
@@ -6,6 +6,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Home - Hotel Spring Plaza</title>
 
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
+  <link rel="icon" type="image" href="Library/images/favicon.png">
+  <link href="Library/css/bootstrap.css" rel="stylesheet">
+
   <link href="Library/css/nifty.css" rel="stylesheet">
 
   <link href="Library/css/demo/nifty-demo-icons.min.css" rel="stylesheet">
